d76ff47400c60b3a94528eb824d8eab197c08d9f
AnshulBompada <ancient.flame1145@gmail.com>
2025-07-06 22:40:21 -0500

    Used cleaner implementation with [-180,180) mapping.

--- MathUtils.cpp
+++ MathUtils.cpp
@@ -21,13 +21,12 @@ double MathUtils::epsilonEquals(double a, double b) {
 double MathUtils::inputModulus(double input, double minInput, double maxInput) {
     double modulus = maxInput - minInput;
 
-    // Wrap input if it's above the maximum input
-    int numMax = (int) ((input - minInput) / modulus);
-    input -= numMax * modulus;
+    // Normalize input relative to minInput
+    double normalized = input - minInput;
 
-    // Wrap input if it's below the minimum input
-    int numMin = (int) ((input - maxInput) / modulus);
-    input -= numMin * modulus;
+    // Wrap using floor (handles negatives correctly)
+    normalized -= std::floor(normalized / modulus) * modulus;
 
-    return input;
+    // Return wrapped input back in [minInput, maxInput)
+    return normalized + minInput;
 }
